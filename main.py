70c60642cfc4a5ea11dffe88d16ade9417a94c42
atsybulkoUp <atsybulka@upteam.com>
2023-03-25 19:57:17 +0400

    update req

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ BATCH_SIZE = 32
 NUM_CLASSES = 2  # Number of skin types
 EPOCHS = 50
 
+
 # Model creation and training
 def load_model():
     global model, class_indices
@@ -24,6 +25,7 @@ def load_model():
         raise ValueError("Model file not found.")
 
     return model, class_indices
+
 
 # Telegram bot handlers
 def start(update: Update, context: CallbackContext):
@@ -39,7 +41,7 @@ def handle_photo(update: Update, context: CallbackContext):
         f.seek(0)
         img = Image.open(f)
         img = img.convert('RGB')
-        img = img.resize((IMG_SIZE, IMG_SIZE), Image.ANTIALIAS)
+        img = img.resize((IMG_SIZE, IMG_SIZE), Image.LANCZOS)
 
     update.message.reply_text('image uploaded')
 
@@ -48,13 +50,29 @@ def handle_photo(update: Update, context: CallbackContext):
     prediction = model.predict(img_batch)
     predicted_class = np.argmax(prediction, axis=1)
 
-    class_mapping = {v: k for k, v in class_indices.items()}
-    update.message.reply_text(class_mapping)
+    # class_mapping = {v: k for k, v in class_indices.items()}
+    update.message.reply_text(class_indices)
     update.message.reply_text(predicted_class)
 
-    skin_type = class_mapping[predicted_class[0]]
+    skin_type = class_indices[predicted_class[0]]
 
     update.message.reply_text(f'The predicted skin type is: {skin_type}')
+
+# def test(image_path: str):
+#     img = Image.open(image_path)
+#     img = img.convert('RGB')
+#     img = img.resize((IMG_SIZE, IMG_SIZE), Image.LANCZOS)
+#
+#     img_array = np.asarray(img) / 255.0
+#     img_batch = np.expand_dims(img_array, axis=0)
+#     prediction = model.predict(img_batch)
+#     predicted_class = np.argmax(prediction, axis=1)
+#
+#     # class_mapping = {v: k for k, v in class_indices.items()}
+#     print(f'{class_indices[predicted_class[0]]}!!!!!!!!!')
+#     skin_type = class_indices[predicted_class[0]]
+#
+#     return skin_type
 
 # Main function
 def main():
@@ -68,17 +86,20 @@ def main():
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     logger = logging.getLogger(__name__)
 
-    # Replace with your API token
-    api_token = '6248741319:AAFGMjQAokh679_lM4PQjwe5xpp2ohgSvco'
-
-    updater = Updater(api_token)
-
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(MessageHandler(filters.Filters.photo, handle_photo))
-
-    updater.start_polling()
-    updater.idle()
+    # image_path = "dry(377).jpg"
+    # skin_type = test(image_path)
+    # print(skin_type)
+    # # Replace with your API token
+    # api_token = '6248741319:AAFGMjQAokh679_lM4PQjwe5xpp2ohgSvco'
+    #
+    # updater = Updater(api_token)
+    #
+    # dp = updater.dispatcher
+    # dp.add_handler(CommandHandler("start", start))
+    # dp.add_handler(MessageHandler(filters.Filters.photo, handle_photo))
+    #
+    # updater.start_polling()
+    # updater.idle()
 
 if __name__ == '__main__':
     main()
